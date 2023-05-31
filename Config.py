5950538b435eb33a91bab7ec0162ef62af490ede
Aliyev Ali <108521240+offlineflood@users.noreply.github.com>
2023-05-31 22:27:53 +0400

    Update and rename DConfig.py to Config.py

--- Config.py
+++ Config.py
@@ -4,6 +4,7 @@ class Config():
     # Bu dəyərləri my.telegram.org saytından əldə edin
     #>>> https://my.telegram.org
     #>>> https://t.me/BotFather
+    
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
